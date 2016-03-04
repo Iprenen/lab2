3079a7e42e9a203489e95fadc9d61205c97fd071
Eric Stenberg <eric.stenberg@telia.com>
2016-03-04 13:00:14 +0100

    compare_and_swap atomic

--- test_cmpxchg.c
+++ test_cmpxchg.c
@@ -55,7 +55,7 @@ increase_atomic(int thread, int iterations, volatile int *data)
          */
 
          for (int i = 0; i < iterations; i++) {
-           while (true) {
+           while (1) {
             if (asm_atomic_cmpxchg_int32((int32_t *) &flag,0,1))
               break;
             while (flag != 0) {};
@@ -74,7 +74,7 @@ decrease_atomic(int thread, int iterations, volatile int *data)
          * atomic compare and exchange instructions. See lab2_asm.h.
          */
          for (int i = 0; i < iterations; i++) {
-           while (true) {
+           while (1) {
             if (asm_atomic_cmpxchg_int32((int32_t *) &flag,0,1))
               break;
             while (flag != 0) {};
