7ffe45f0ca92b1991467bbe5d02ebab4a514ba69
Eric Stenberg <eric.stenberg@telia.com>
2016-03-07 13:44:05 +0100

    derp

---
+++ run.sh
@@ -0,0 +1,51 @@
+#!bin/bash
+
+make clean
+make
+
+clear
+
+echo "Pthreads tests"
+echo "critical"
+./lab2 -t critical -c pthreads
+./lab2 -t critical4 -c pthreads
+./lab2 -t critical8 -c pthreads
+echo "incdec"
+./lab2 -t incdec_no_atomic -c pthreads
+./lab2 -t incdec_atomic -c pthreads
+echo "cmpxchg"
+./lab2 -t cmpxchg_atomic -c pthreads
+./lab2 -t cmpxchg_no_atomic -c pthreads
+
+
+echo "\n"
+
+echo "Dekker tests"
+
+echo "critical"
+./lab2 -t critical -c dekker
+./lab2 -t critical4 -c dekker
+./lab2 -t critical8 -c dekker
+
+echo "incdec"
+./lab2 -t incdec_no_atomic -c dekker
+./lab2 -t incdec_atomic -c dekker
+echo "cmpxchg"
+./lab2 -t cmpxchg_atomic -c dekker
+./lab2 -t cmpxchg_no_atomic -c dekker
+
+echo "\n"
+
+echo "CLH tests"
+
+echo "critical"
+./lab2 -t critical -c queue
+./lab2 -t critical4 -c queue
+./lab2 -t critical8 -c queue
+
+echo "incdec"
+./lab2 -t incdec_no_atomic -c queue
+./lab2 -t incdec_atomic -c queue
+echo "cmpxchg"
+./lab2 -t cmpxchg_atomic -c queue
+./lab2 -t cmpxchg_no_atomic -c queue
